7e7db3b6c8be17442bce9fef25d421ba158bbc72
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-01 00:26:13 +0000

    p1

--- code/infra/script_deploy.sh
+++ code/infra/script_deploy.sh
@@ -1,1 +1,4 @@
-aws cloudformation create-stack --debug --stack-name MyCognitoUserPoolStack --template-body ./cognito_user_pool.json
+export stack_name="MyCognitoUserPoolStack"
+./script_cleanup.sh $stack_name
+# aws cloudformation deploy --stack-name $stack_name --template-file cf_cognito_user_pool.json 
+aws cloudformation deploy --stack-name $stack_name --template-file cf_s3_website.json 
