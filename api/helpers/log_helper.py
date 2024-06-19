19ae2a7f8eeb6c2383f2b13a47070b0106eefeac
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-19 06:32:29 +0000

    uni tests

--- api/helpers/log_helper.py
+++ api/helpers/log_helper.py
@@ -1,3 +1,8 @@
+"""
+This module provides helper functions for setting and resetting standard log fields,
+and preparing the log string with the current values of the standard log fields.
+"""
+
 _xray_trace_id = "Undefined"
 _trace_id = "Undefined"
 _parent_span_id = "Undefined"
@@ -11,54 +16,94 @@ _service_name = "Undefined"
 _operation_name = "Undefined"
 _api_version = "Undefined"
 
-def set_standard_log_fields(xray_trace_id = None,
-                            trace_id = None,
-                            parent_span_id = None,
-                            span_id = None,
-                            session_id = None,
-                            organisation_id = None,
-                            organisation_name = None,
-                            user_id = None,
-                            customer_id = None,
-                            service_name = None,
-                            operation_name = None,
-                            api_version = None):
-    if _xray_trace_id is not None:
+def set_standard_log_fields(xray_trace_id=None, trace_id=None, parent_span_id=None, span_id=None, session_id=None,
+                            organisation_id=None, organisation_name=None, user_id=None, customer_id=None,
+                            service_name=None, operation_name=None, api_version=None):
+    """
+    Sets the standard log fields with the provided values.
+
+    Args:
+        xray_trace_id (str): The X-Ray trace ID.
+        trace_id (str): The trace ID.
+        parent_span_id (str): The parent span ID.
+        span_id (str): The span ID.
+        session_id (str): The session ID.
+        organisation_id (str): The organisation ID.
+        organisation_name (str): The organisation name.
+        user_id (str): The user ID.
+        customer_id (str): The customer ID.
+        service_name (str): The service name.
+        operation_name (str): The operation name.
+        api_version (str): The API version.
+    """
+    global _xray_trace_id, _trace_id, _parent_span_id, _span_id, _session_id, _organisation_id, _organisation_name, \
+        _user_id, _customer_id, _service_name, _operation_name, _api_version
+
+    if xray_trace_id is not None:
         _xray_trace_id = xray_trace_id
-    if _trace_id is not None:
+    if trace_id is not None:
         _trace_id = trace_id
-    if _parent_span_id is not None:
+    if parent_span_id is not None:
         _parent_span_id = parent_span_id
-    if _span_id is not None:
+    if span_id is not None:
         _span_id = span_id
-    if _session_id is not None:
+    if session_id is not None:
         _session_id = session_id
-    if _organisation_id is not None:
+    if organisation_id is not None:
         _organisation_id = organisation_id
-    if _organisation_name is not None:
+    if organisation_name is not None:
         _organisation_name = organisation_name
-    if _user_id is not None:
+    if user_id is not None:
         _user_id = user_id
-    if _customer_id is not None:
+    if customer_id is not None:
         _customer_id = customer_id
-    if _service_name is not None:
+    if service_name is not None:
         _service_name = service_name
-    if _operation_name is not None:
+    if operation_name is not None:
         _operation_name = operation_name
-    if _api_version is not None:
+    if api_version is not None:
         _api_version = api_version
 
 def prepare_log_string():
-    log_string =    f"XrayTraceId: {_xray_trace_id}," + \
-                    f"TraceId: {_trace_id}," + \
-                    f"ParentSpanId: {_parent_span_id}," + \
-                    f"SpanId: {_span_id}," + \
-                    f"SessionId: {_session_id}," + \
-                    f"OrganisationId: {_organisation_id}," + \
-                    f"OrganisationName: {_organisation_name}," + \
-                    f"UserId: {_user_id}," + \
-                    f"CustomerId: {_customer_id}," + \
-                    f"ServiceName: {_service_name}," + \
-                    f"OperationName: {_operation_name}," + \
-                    f"ApiVersion: {_api_version}"
+    """
+    Prepares the log string with the current values of the standard log fields.
+
+    Returns:
+        str: The log string.
+    """
+    log_string = f"XRayTraceId: {_xray_trace_id}," + \
+                 f"TraceId: {_trace_id}," + \
+                 f"ParentSpanId: {_parent_span_id}," + \
+                 f"SpanId: {_span_id}," + \
+                 f"SessionId: {_session_id}," + \
+                 f"OrganisationId: {_organisation_id}," + \
+                 f"OrganisationName: {_organisation_name}," + \
+                 f"UserId: {_user_id}," + \
+                 f"CustomerId: {_customer_id}," + \
+                 f"ServiceName: {_service_name}," + \
+                 f"OperationName: {_operation_name}," + \
+                 f"APIVersion: {_api_version}"
     return log_string
+
+
+def reset_standard_log_fields():
+    """
+    Resets all the standard log fields to their default value of "Undefined".
+    Should only be used for unit testing
+    """
+    global _xray_trace_id, _trace_id, _parent_span_id, _span_id, _session_id, _organisation_id, _organisation_name, \
+        _user_id, _customer_id, _service_name, _operation_name, _api_version
+
+    _xray_trace_id = "Undefined"
+    _trace_id = "Undefined"
+    _parent_span_id = "Undefined"
+    _span_id = "Undefined"
+    _session_id = "Undefined"
+    _organisation_id = "Undefined"
+    _organisation_name = "Undefined"
+    _user_id = "Undefined"
+    _customer_id = "Undefined"
+    _service_name = "Undefined"
+    _operation_name = "Undefined"
+    _api_version = "Undefined"
+
--- api/helpers/test/test_log_helper.py
+++ api/helpers/test/test_log_helper.py
@@ -1,57 +1,12 @@
 import pytest
-from api.helpers.log_helper import set_standard_log_fields, prepare_log_string
+from helpers.log_helper import set_standard_log_fields, prepare_log_string, reset_standard_log_fields
+import helpers.log_helper as log_helper
 
-def test_set_standard_log_fields():
-    # Test setting all log fields
-    set_standard_log_fields(
-        xray_trace_id="123",
-        trace_id="456",
-        parent_span_id="789",
-        span_id="012",
-        session_id="345",
-        organisation_id="678",
-        organisation_name="TestOrg",
-        user_id="901",
-        customer_id="234",
-        service_name="TestService",
-        operation_name="TestOperation",
-        api_version="1.0"
-    )
-    assert _xray_trace_id == "123"
-    assert _trace_id == "456"
-    assert _parent_span_id == "789"
-    assert _span_id == "012"
-    assert _session_id == "345"
-    assert _organisation_id == "678"
-    assert _organisation_name == "TestOrg"
-    assert _user_id == "901"
-    assert _customer_id == "234"
-    assert _service_name == "TestService"
-    assert _operation_name == "TestOperation"
-    assert _api_version == "1.0"
-
-    # Test setting only some log fields
-    set_standard_log_fields(
-        xray_trace_id="123",
-        organisation_id="678",
-        user_id="901",
-        service_name="TestService",
-        api_version="1.0"
-    )
-    assert _xray_trace_id == "123"
-    assert _trace_id == "Undefined"
-    assert _parent_span_id == "Undefined"
-    assert _span_id == "Undefined"
-    assert _session_id == "Undefined"
-    assert _organisation_id == "678"
-    assert _organisation_name == "Undefined"
-    assert _user_id == "901"
-    assert _customer_id == "Undefined"
-    assert _service_name == "TestService"
-    assert _operation_name == "Undefined"
-    assert _api_version == "1.0"
-
-def test_prepare_log_string():
+@pytest.fixture(autouse=True)
+def setup():
+    reset_standard_log_fields()
+    
+def test_prepare_log_string_full():
     # Test preparing log string with all log fields set
     set_standard_log_fields(
         xray_trace_id="123",
@@ -68,9 +23,11 @@ def test_prepare_log_string():
         api_version="1.0"
     )
     log_string = prepare_log_string()
-    expected_log_string = "X-Ray Trace ID: 123, Trace ID: 456, Parent Span ID: 789, Span ID: 012, Session ID: 345, Organisation ID: 678, Organisation Name: TestOrg, User ID: 901, Customer ID: 234, Service Name: TestService, Operation Name: TestOperation, API Version: 1.0"
+    expected_log_string = "XRayTraceId: 123,TraceId: 456,ParentSpanId: 789,SpanId: 012,SessionId: 345,OrganisationId: 678,OrganisationName: TestOrg,UserId: 901,CustomerId: 234,ServiceName: TestService,OperationName: TestOperation,APIVersion: 1.0"
     assert log_string == expected_log_string
 
+
+def test_prepare_log_string_partial():
     # Test preparing log string with some log fields missing
     set_standard_log_fields(
         xray_trace_id="123",
@@ -80,5 +37,5 @@ def test_prepare_log_string():
         api_version="1.0"
     )
     log_string = prepare_log_string()
-    expected_log_string = "X-Ray Trace ID: 123, Trace ID: Undefined, Parent Span ID: Undefined, Span ID: Undefined, Session ID: Undefined, Organisation ID: 678, Organisation Name: Undefined, User ID: 901, Customer ID: Undefined, Service Name: TestService, Operation Name: Undefined, API Version: 1.0"
+    expected_log_string = "XRayTraceId: 123,TraceId: Undefined,ParentSpanId: Undefined,SpanId: Undefined,SessionId: Undefined,OrganisationId: 678,OrganisationName: Undefined,UserId: 901,CustomerId: Undefined,ServiceName: TestService,OperationName: Undefined,APIVersion: 1.0"
     assert log_string == expected_log_string
