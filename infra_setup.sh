48a8f4ef802acda0f36b9ce28cb11b5e909952eb
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-22 08:51:32 +0000

    infra

--- infra_setup.sh
+++ infra_setup.sh
@@ -7,6 +7,10 @@ sudo ./aws/install
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository 'deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main'
 sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
 sudo apt-get install terraform -y
 wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.35.0/terragrunt_linux_amd64
 chmod +x terragrunt_linux_amd64
