1788b79081a1d1431bc75ecfe6d064168b615a26
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-22 08:31:31 +0000

    infra

--- infra_setup.sh
+++ infra_setup.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install unzip curl -y 
 curl 'https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
 unzip -o awscliv2.zip
+rm awscliv2.zip
 sudo ./aws/install 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository 'deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main'
@@ -9,4 +10,4 @@ sudo apt-get update
 sudo apt-get install terraform -y
 wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.35.0/terragrunt_linux_amd64
 chmod +x terragrunt_linux_amd64
-sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt"
+sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
