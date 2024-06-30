0686455f83981c657573f8c2d75fb0803942d042
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-30 20:21:07 +0000

    fix response

--- api/api_name_example/handler.py
+++ api/api_name_example/handler.py
@@ -3,14 +3,7 @@ from api.helpers.lambda_helpers.lambda_handler import LambdaHandler
 basic_request_model = {
     "type": "object",
     "properties": {
-        "key1": {
-            "type": "string"
-        },
-        "key2": {
-            "type": "string"
-        }
-    },
-    "required": ["key1", "key2"]
+    }
 }
 
 def handler(event, context):
