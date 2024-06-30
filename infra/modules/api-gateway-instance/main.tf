2473c58bc05dbbc40572e9f00046ea300b527685
pwgith <131629357+pwgith@users.noreply.github.com>
2024-06-30 06:33:40 +0000

    api gw

--- infra/modules/api-gateway-instance/main.tf
+++ infra/modules/api-gateway-instance/main.tf
@@ -6,6 +6,8 @@ provider "aws" {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 # Create API Gateway REST API
 resource "aws_api_gateway_rest_api" "example_api" {
   name        = "example-api"
@@ -16,7 +18,7 @@ resource "aws_api_gateway_rest_api" "example_api" {
 resource "aws_api_gateway_resource" "example_resource" {
   rest_api_id = aws_api_gateway_rest_api.example_api.id
   parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
-  path_part   = "items"
+  path_part   = "sample"
 }
 
 # Create GET Method
@@ -25,10 +27,6 @@ resource "aws_api_gateway_method" "example_method" {
   resource_id   = aws_api_gateway_resource.example_resource.id
   http_method   = "GET"
   authorization = "NONE"
-
-  request_parameters = {
-    "method.request.header.Content-Type" = true
-  }
 }
 
 # Create Integration
@@ -36,22 +34,45 @@ resource "aws_api_gateway_integration" "example_integration" {
   rest_api_id             = aws_api_gateway_rest_api.example_api.id
   resource_id             = aws_api_gateway_resource.example_resource.id
   http_method             = aws_api_gateway_method.example_method.http_method
-  integration_http_method = "GET"
-  type                    = "AWS"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
   uri                     = var.api_integration_invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "example_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
-  resource_id = aws_api_gateway_resource.example_resource.id
-  http_method = aws_api_gateway_method.example_method.http_method
-
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_method.http_method}${aws_api_gateway_resource.example_resource.path}"
 }
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example_api.body))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [aws_api_gateway_method.example_method, aws_api_gateway_integration.example_integration]
+}
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  stage_name    = "dev"
+}
+
+# resource "aws_api_gateway_method_response" "example_method_response" {
+#   rest_api_id = aws_api_gateway_rest_api.example_api.id
+#   resource_id = aws_api_gateway_resource.example_resource.id
+#   http_method = aws_api_gateway_method.example_method.http_method
+
+#   status_code = "200"
+
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin" = true
+#   }
+# }
 
 # resource "aws_api_gateway_integration_response" "example_integration_response" {
 #   rest_api_id = aws_api_gateway_rest_api.example_api.id
